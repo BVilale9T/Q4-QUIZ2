615bd76a9c5f791a7f8b846f5fc3f39c636676fe
BVilale9T <vilale.bi@obmontessori.edu.ph>
2025-05-21 22:28:03 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
-// AVERAGE RATING JS EVENT
-    function GradeAnalyzer(){
+function GradeAnalyzer(){
         var Sub_Grade1 = parseInt(document.getElementById("gr_1id").value);
         var Sub_Grade2 = parseInt(document.getElementById("gr_2id").value);
         var Sub_Grade3 = parseInt(document.getElementById("gr_3id").value);
 
-        // Calculate Average
         let Average = ((Sub_Grade1 + Sub_Grade2 + Sub_Grade3) / 3).toFixed(2);
 
-        // Average Grade & Rating
         if (Average < 70){
             window.alert("Your Average is " + Average + " - Failed");
         } else if(Average >= 70 && Average <= 74.99){
@@ -20,13 +17,11 @@
             window.alert("Your Average is " + Average + " - Above Satisfactory");
         } else if(Average >= 94 && Average <= 100){
             window.alert("Your Average is " + Average + " - Excellent");
-        // Invalid or no values put
         } else{
             window.alert("Error: Please enter valid numbers for all subjects.");
         }
     }
 
-// REFRESH JS EVENT
     function Refresh(){
         window.location.reload();
     }
